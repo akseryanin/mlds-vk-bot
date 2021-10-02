3f2d677659e099c7ea9ea2397aacbcb7bcb3bd59
akseryanin <sasha.seryanin@yandex.ru>
2021-10-02 17:44:28 +0300

    Первая часть проекта

---
+++ src/Main.py
@@ -0,0 +1,26 @@
+from vkbottle.bot import Bot, Message
+from vkbottle import API
+from datetime import datetime
+import secrets
+
+api = API(token=secrets.API_TOKEN_USER)
+bot = Bot(token=secrets.API_TOKEN_BOT)
+
+
+def getTime(time):
+    return str(datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'))
+
+
+@bot.on.message(text="стат")
+async def messages_handler(message: Message) -> str:
+    response = await api.wall.get(domain=secrets.domain, count=10, filter="owner")
+    answer = ""
+    for post in response.items:
+        answer += f"Дата поста: {getTime(post.date)}\n"
+        answer += f"Количество лайков под постом: {post.likes.count}\n\n"
+    answer += f"\nВсего обработано постов: {len(response.items)}"
+    await message.answer(answer)
+    return answer
+
+
+bot.run_forever()

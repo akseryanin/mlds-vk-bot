27eb2b465302961ec1bd4880f359809cd6181a0c
akseryanin <sasha.seryanin@yandex.ru>
2021-10-09 14:50:05 +0300

    try heroku secrets

--- src/Main.py
+++ src/Main.py
@@ -6,10 +6,15 @@ import random
 from enums.Metrics import Metric
 from enums.questions import Question
 from models.response import Response
+from boto.s3.connection import S3Connection
 
-API_TOKEN_USER = os.environ.get('API_TOKEN_USER')
-API_TOKEN_BOT = os.environ.get('API_TOKEN_BOT')
-domain = os.environ.get('domain')
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+bucket = s3.create_bucket('bucket')
+
+API_TOKEN_USER = bucket.get('API_TOKEN_USER')
+API_TOKEN_BOT = bucket.get('API_TOKEN_BOT')
+domain = bucket.get('domain')
+print(API_TOKEN_BOT, API_TOKEN_USER, domain)
 
 api = API(token=API_TOKEN_USER)
 bot = Bot(token=API_TOKEN_BOT)
